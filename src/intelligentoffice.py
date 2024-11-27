8d22998f455ed816fcd12f871a06320b8de1fd60
Merenda Franco N <merenda.franco83@gmail.com>
2024-11-27 16:28:20 +0100

    US3 - Green Phase

--- src/intelligentoffice.py
+++ src/intelligentoffice.py
@@ -71,9 +71,12 @@ class IntelligentOffice:
                 self.blinds_open = False
 
     def manage_light_level(self) -> None:
-        # To be implemented
-        pass
-
+        if self.ambient_light_sensor.lux < 500:
+            GPIO.output(self.LED_PIN, True)
+            self.light_on = True
+        elif self.ambient_light_sensor.lux > 550:
+            GPIO.output(self.LED_PIN, False)
+            self.light_on = False
 
     def monitor_air_quality(self) -> None:
         # To be implemented

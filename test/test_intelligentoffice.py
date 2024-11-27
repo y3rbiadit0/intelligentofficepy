2a334ee315a1aea0f9133058041f573f31e493c8
Merenda Franco N <merenda.franco83@gmail.com>
2024-11-27 17:00:38 +0100

    Refactor - Reformat file

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -1,18 +1,21 @@
 import unittest
 from datetime import datetime
 from unittest.mock import patch, Mock, PropertyMock
+
 import mock.GPIO as GPIO
 from mock.SDL_DS3231 import SDL_DS3231
-from mock.adafruit_veml7700 import VEML7700
 from src.intelligentoffice import IntelligentOffice, IntelligentOfficeError
 
 
 class TestIntelligentOffice(unittest.TestCase):
-    
+
     def setUp(self):
         self.intelligent_office = IntelligentOffice()
-        self.infrared_pins = [self.intelligent_office.INFRARED_PIN1, self.intelligent_office.INFRARED_PIN2, self.intelligent_office.INFRARED_PIN3, self.intelligent_office.INFRARED_PIN4]
-    
+        self.infrared_pins = [self.intelligent_office.INFRARED_PIN1,
+                              self.intelligent_office.INFRARED_PIN2,
+                              self.intelligent_office.INFRARED_PIN3,
+                              self.intelligent_office.INFRARED_PIN4]
+
     @patch.object(GPIO, "input")
     def test_check_quadrant_occupancy_occupied(self, infrared_sensor_mock: Mock):
         infrared_sensor_mock.return_value = True
@@ -32,27 +35,29 @@ class TestIntelligentOffice(unittest.TestCase):
             self.assertFalse(is_occupied)
 
     def test_check_quadrant_occupancy_not_valid_pin(self):
-        
+
         not_valid_infrared_pin = self.intelligent_office.LED_PIN
 
-        self.assertRaises(IntelligentOfficeError, self.intelligent_office.check_quadrant_occupancy, not_valid_infrared_pin)
+        self.assertRaises(IntelligentOfficeError,
+                          self.intelligent_office.check_quadrant_occupancy,
+                          not_valid_infrared_pin)
 
     @patch.object(IntelligentOffice, "change_servo_angle")
     @patch.object(SDL_DS3231, "read_datetime")
-    def test_manage_blinds_based_on_time_open(self, read_datetime_mock: Mock, change_servo_angle_mock: Mock):
+    def test_manage_blinds_based_on_time_open(self, read_datetime_mock: Mock,
+                                              change_servo_angle_mock: Mock):
         read_datetime_mock.return_value = datetime(2024, 10, 7, 8, 0, 0)
-        
 
         self.intelligent_office.manage_blinds_based_on_time()
-        change_servo_angle_mock.assert_called_once_with(12) # (180/18) + 2
+        change_servo_angle_mock.assert_called_once_with(12)  # (180/18) + 2
         self.assertTrue(self.intelligent_office.blinds_open)
 
     @patch.object(IntelligentOffice, "change_servo_angle")
     @patch.object(SDL_DS3231, "read_datetime")
     def test_manage_blinds_based_on_time_closes(self, read_datetime_mock: Mock,
-                                              change_servo_angle_mock: Mock):
+                                                change_servo_angle_mock: Mock):
         read_datetime_mock.return_value = datetime(2024, 10, 10, 20, 0, 0)
-        
+
         self.intelligent_office.blinds_open = True
 
         self.intelligent_office.manage_blinds_based_on_time()
@@ -62,42 +67,47 @@ class TestIntelligentOffice(unittest.TestCase):
     @patch.object(IntelligentOffice, "change_servo_angle")
     @patch.object(SDL_DS3231, "read_datetime")
     def test_manage_blinds_based_on_time_saturday_sunday(self, read_datetime_mock: Mock,
-                                                change_servo_angle_mock: Mock):
+                                                         change_servo_angle_mock: Mock):
 
-        read_datetime_mock.side_effect = [datetime(2024, 10, 5, 20, 0, 0), datetime(2024, 10, 6, 20, 0, 0)]
-        
+        read_datetime_mock.side_effect = [datetime(2024, 10, 5, 20, 0, 0),
+                                          datetime(2024, 10, 6, 20, 0, 0)]
+
         for _ in read_datetime_mock.side_effect:
             self.intelligent_office.manage_blinds_based_on_time()
             change_servo_angle_mock.assert_not_called()  # (0/18) + 2
             self.assertFalse(self.intelligent_office.blinds_open)
 
-
     @patch.object(GPIO, "input")
     @patch.object(GPIO, "output")
-    def test_manage_light_level_turn_on(self, led_sensor_mock: Mock, infrared_sensor_mock: Mock):
+    def test_manage_light_level_turn_on(self, led_sensor_mock: Mock,
+                                        infrared_sensor_mock: Mock):
         infrared_sensor_mock.return_value = True
         with patch("mock.adafruit_veml7700.VEML7700.lux", PropertyMock()) as mock_lux:
             mock_lux.return_value = 499.0
 
             self.intelligent_office.manage_light_level()
 
-            led_sensor_mock.assert_called_once_with(self.intelligent_office.LED_PIN, True)
+            led_sensor_mock.assert_called_once_with(self.intelligent_office.LED_PIN,
+                                                    True)
             self.assertTrue(self.intelligent_office.light_on)
 
     @patch.object(GPIO, "input")
     @patch.object(GPIO, "output")
-    def test_manage_light_level_turn_off(self, led_sensor_mock: Mock, infrared_sensor_mock: Mock):
+    def test_manage_light_level_turn_off(self, led_sensor_mock: Mock,
+                                         infrared_sensor_mock: Mock):
         infrared_sensor_mock.return_value = True
         with patch("mock.adafruit_veml7700.VEML7700.lux", PropertyMock()) as mock_lux:
             mock_lux.return_value = 551.0
 
             self.intelligent_office.manage_light_level()
 
-            led_sensor_mock.assert_called_once_with(self.intelligent_office.LED_PIN, False)
+            led_sensor_mock.assert_called_once_with(self.intelligent_office.LED_PIN,
+                                                    False)
             self.assertFalse(self.intelligent_office.light_on)
 
     @patch.object(GPIO, "input")
-    def test_manage_light_level_turn_off_light_when_all_workers_off(self, infrared_sensor_mock: Mock, ):
+    def test_manage_light_level_turn_off_light_when_all_workers_off(self,
+                                                                    infrared_sensor_mock: Mock, ):
         with patch("mock.adafruit_veml7700.VEML7700.lux", PropertyMock()) as mock_lux:
             mock_lux.return_value = 499.0
             infrared_sensor_mock.return_value = False
@@ -109,7 +119,8 @@ class TestIntelligentOffice(unittest.TestCase):
             self.assertFalse(self.intelligent_office.light_on)
 
     @patch.object(GPIO, "input")
-    def test_manage_light_level_turn_off_light_when_all_workers_off(self, infrared_sensor_mock: Mock, ):
+    def test_manage_light_level_turn_off_light_when_all_workers_off(self,
+                                                                    infrared_sensor_mock: Mock, ):
         with patch("mock.adafruit_veml7700.VEML7700.lux", PropertyMock()) as mock_lux:
             mock_lux.return_value = 499.0
             infrared_sensor_mock.return_value = True
@@ -119,10 +130,10 @@ class TestIntelligentOffice(unittest.TestCase):
 
             self.assertTrue(self.intelligent_office.light_on)
 
-
     @patch.object(GPIO, "input")
     @patch.object(GPIO, "output")
-    def test_monitor_light_turn_on_buzzer_if_gas_detected(self, buzzer_mock: Mock, gas_smoke_detector: Mock):
+    def test_monitor_light_turn_on_buzzer_if_gas_detected(self, buzzer_mock: Mock,
+                                                          gas_smoke_detector: Mock):
         gas_smoke_detector.return_value = True
         self.intelligent_office.monitor_air_quality()
 
@@ -132,7 +143,8 @@ class TestIntelligentOffice(unittest.TestCase):
 
     @patch.object(GPIO, "input")
     @patch.object(GPIO, "output")
-    def test_monitor_light_turn_off_buzzer_if_gas_not_detected(self, buzzer_mock: Mock, gas_smoke_detector: Mock):
+    def test_monitor_light_turn_off_buzzer_if_gas_not_detected(self, buzzer_mock: Mock,
+                                                               gas_smoke_detector: Mock):
         gas_smoke_detector.return_value = False
         self.intelligent_office.monitor_air_quality()
 

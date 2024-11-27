a0a5d2044e7b170b40a783882531de1488b241e0
Merenda Franco N <merenda.franco83@gmail.com>
2024-11-27 16:00:25 +0100

    US1 - Refactor Phase - Green Phase

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -10,10 +10,35 @@ from src.intelligentoffice import IntelligentOffice, IntelligentOfficeError
 class TestIntelligentOffice(unittest.TestCase):
 
     @patch.object(GPIO, "input")
-    def test_check_quadrant_occupancy(self, infrared_sensor_quadrant_1: Mock):
-        infrared_sensor_quadrant_1.return_value = True
+    def test_check_quadrant_occupancy_occupied(self, infrared_sensor_mock: Mock):
+        infrared_sensor_mock.return_value = True
         intelligent_office = IntelligentOffice()
 
-        is_occupied = intelligent_office.check_quadrant_occupancy(intelligent_office.INFRARED_PIN1)
-        infrared_sensor_quadrant_1.assert_called_once_with(intelligent_office.INFRARED_PIN1)
-        self.assertTrue(is_occupied)
+        infrared_pins = [intelligent_office.INFRARED_PIN1 ,intelligent_office.INFRARED_PIN2 ,intelligent_office.INFRARED_PIN3, intelligent_office.INFRARED_PIN4]
+        for infrared_pin in infrared_pins:
+            is_occupied = intelligent_office.check_quadrant_occupancy(infrared_pin)
+            infrared_sensor_mock.assert_called_with(infrared_pin)
+            self.assertTrue(is_occupied)
+
+    @patch.object(GPIO, "input")
+    def test_check_quadrant_occupancy_free(self, infrared_sensor_mock: Mock):
+        infrared_sensor_mock.return_value = False
+        intelligent_office = IntelligentOffice()
+
+        infrared_pins = [intelligent_office.INFRARED_PIN1,
+                         intelligent_office.INFRARED_PIN2,
+                         intelligent_office.INFRARED_PIN3,
+                         intelligent_office.INFRARED_PIN4]
+        for infrared_pin in infrared_pins:
+            is_occupied = intelligent_office.check_quadrant_occupancy(infrared_pin)
+            infrared_sensor_mock.assert_called_with(infrared_pin)
+            self.assertFalse(is_occupied)
+
+    @patch.object(GPIO, "input")
+    def test_check_quadrant_occupancy_not_valid_pin(self, infrared_sensor_mock: Mock):
+        infrared_sensor_mock.return_value = True
+        intelligent_office = IntelligentOffice()
+        not_valid_infrared_pin = intelligent_office.LED_PIN
+
+        self.assertRaises(IntelligentOfficeError, intelligent_office.check_quadrant_occupancy, not_valid_infrared_pin)
+
